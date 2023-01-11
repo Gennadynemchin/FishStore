b79f4304d19b3ae421337cb727bf6d15abc0f4be
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-01-11 12:40:50 +0300

    Added get_all_products func;

--- elasticpath.py
+++ elasticpath.py
@@ -1,4 +1,5 @@
 import os
+import json
 import requests
 from dotenv import load_dotenv
 
@@ -15,13 +16,27 @@ def get_client_token(client_id, client_secret, store_id):
     return response.json()
 
 
+def get_all_products(token, store_id):
+    url = "https://useast.api.elasticpath.com/pcm/products"
+    headers = {'accept': 'application/json',
+               'content-type': 'application/json',
+               'x-moltin-auth-store': store_id,
+               'Authorization': f'Bearer {token}'}
+    response = requests.request("GET", url, headers=headers)
+    return response.json()
+
+
+
+
 def main():
     load_dotenv()
     client_id = os.getenv('CLIENT_ID')
     client_secret = os.getenv('CLIENT_SECRET')
     store_id = os.getenv('STORE_ID')
+    token = os.getenv('TOKEN')
 
-    print(get_client_token(client_id, client_secret, store_id))
+    #print(get_client_token(client_id, client_secret, store_id))
+    print(get_all_products(token, store_id))
 
 
 if __name__ == '__main__':
