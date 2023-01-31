7eeaf916894b08161b829a7ca731d6f8ef2ad076
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-01-31 22:48:53 +0300

    Added product variations;

--- tgbot.py
+++ tgbot.py
@@ -45,29 +45,29 @@ def handle_menu(bot, update, token_filename, store_id, client_id, client_secret)
         keyboard.append([InlineKeyboardButton(product_name, callback_data=product_id)])
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.effective_message.delete()
-    update.effective_message.reply_text(text=f"Let's choose:",
-                                        reply_markup=reply_markup)
+    update.effective_message.reply_text(text=f"Let's choose:", reply_markup=reply_markup)
     return State.HANDLE_DESCRIPTION
 
 
 def handle_description(bot, update, token_filename, store_id, client_id, client_secret):
-    query = update.callback_query
-    product_id = query.data
     if is_token_expired(token_filename, store_id):
         new_token = get_client_token(client_id, client_secret, store_id)['access_token']
         set_elasticpath_token(new_token, token_filename)
     elasticpath_token = get_elasticpath_token(token_filename)
+    product_id = update.callback_query.data
     product_info = get_product_by_id(elasticpath_token, product_id, store_id)
     product_name = product_info['data']['attributes']['name']
     product_price = product_info['data']['meta']['display_price']['with_tax']['formatted']
     product_sku = product_info['data']['attributes']['sku']
     photo_link = get_photo_by_productid(elasticpath_token, product_id, store_id)
-    keyboard = [[InlineKeyboardButton('Add to cart', callback_data=f'add_to_cart {product_id}')],
+    keyboard = [[InlineKeyboardButton('Buy 1kg', callback_data=f'add_to_cart {product_id} 1'),
+                 InlineKeyboardButton('Buy 3kg', callback_data=f'add_to_cart {product_id} 3'),
+                 InlineKeyboardButton('Buy 10kg', callback_data=f'add_to_cart {product_id} 10')],
                 [InlineKeyboardButton('Go to cart', callback_data='cart_info')],
                 [InlineKeyboardButton('Back', callback_data='back')]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.effective_message.delete()
-    bot.send_photo(chat_id=query.message.chat_id,
+    bot.send_photo(chat_id=update.callback_query.message.chat_id,
                    caption=f'{product_name}\n{product_price}\n{product_sku}',
                    photo=photo_link,
                    reply_markup=reply_markup)
@@ -75,21 +75,19 @@ def handle_description(bot, update, token_filename, store_id, client_id, client_
 
 
 def add_to_cart(bot, update, token_filename, store_id, client_id, client_secret):
-    query = update.callback_query
     if is_token_expired(token_filename, store_id):
         new_token = get_client_token(client_id, client_secret, store_id)['access_token']
         set_elasticpath_token(new_token, token_filename)
     elasticpath_token = get_elasticpath_token(token_filename)
     cart_id = update.effective_user.id
-    product_id = query.data.split(' ')[1]
-    quantity = 1
+    product_id = update.callback_query.data.split(' ')[1]
+    quantity = int(update.callback_query.data.split(' ')[2])
     add_product_to_cart(elasticpath_token, cart_id, store_id, product_id, quantity)
-    query.answer(text='The product has been added to cart', show_alert=True)
+    update.callback_query.answer(text='The product has been added to cart', show_alert=False)
     return State.HANDLE_DESCRIPTION
 
 
 def handle_cart_info(bot, update, token_filename, store_id, client_id, client_secret):
-    query = update.callback_query
     cart_id = update.effective_user.id
     if is_token_expired(token_filename, store_id):
         new_token = get_client_token(client_id, client_secret, store_id)['access_token']
@@ -114,14 +112,13 @@ def handle_cart_info(bot, update, token_filename, store_id, client_id, client_se
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.effective_message.delete()
     bot.send_message(text=f'{" ".join(products_in_cart_info)}\n Total: {total_price}',
-                     chat_id=query.message.chat_id,
-                     message_id=query.message.message_id,
+                     chat_id=update.callback_query.message.chat_id,
+                     message_id=update.callback_query.message.message_id,
                      reply_markup=reply_markup)
     return State.HANDLE_CART
 
 
 def handle_remove_all_from_cart(bot, update, token_filename, store_id, client_id, client_secret):
-    query = update.callback_query
     cart_id = update.effective_user.id
     if is_token_expired(token_filename, store_id):
         new_token = get_client_token(client_id, client_secret, store_id)['access_token']
@@ -129,12 +126,12 @@ def handle_remove_all_from_cart(bot, update, token_filename, store_id, client_id
     elasticpath_token = get_elasticpath_token(token_filename)
     remove_all_from_cart(elasticpath_token, cart_id, store_id)
     keyboard = [[InlineKeyboardButton('Menu', callback_data='menu')]]
-    query.answer(text='The product has been added to cart', show_alert=False)
+    update.callback_query.answer(text='The product has been added to cart', show_alert=False)
     update.effective_message.delete()
     reply_markup = InlineKeyboardMarkup(keyboard)
-    bot.send_message(text='Now your cart is empty. Please go to "Menu"',
-                     chat_id=query.message.chat_id,
-                     message_id=query.message.message_id,
+    bot.send_message(text='Now your cart is empty. Please go to the "Menu"',
+                     chat_id=update.callback_query.message.chat_id,
+                     message_id=update.callback_query.message.message_id,
                      reply_markup=reply_markup)
     return State.HANDLE_CART
 
@@ -223,7 +220,11 @@ def main():
                                                                  client_id=client_id,
                                                                  client_secret=client_secret))
                                     ]},
-        fallbacks=[])
+        fallbacks=[CommandHandler('start', partial(handle_menu,
+                                                   token_filename=token_filename,
+                                                   store_id=store_id,
+                                                   client_id=client_id,
+                                                   client_secret=client_secret))])
 
     dp.add_handler(conv_handler)
     updater.start_polling()
